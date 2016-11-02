fa5ff039d4536c711df4c09edec1db782359ae96
Ryan Christiani <ryan@ryanchristiani.com>
2016-11-02 17:45:48 -0400

    Adds random combo and shows keys

--- js/script.js
+++ js/script.js
@@ -10,11 +10,70 @@ app.keyCodes = {
 	'91': 'CMD'
 };
 
-app.combos = ['Save','Cut','Copy','Select All','Paste','Refresh'];
+app.combos = [
+	{
+		title: 'Save',
+		combo: ['CMD','S']
+	},
+	{
+		title: 'Cut',
+		combo: ['CMD','X']
+	},
+	{
+		title: 'Copy',
+		combo: ['CMD', 'C']
+	},
+	{
+		title: 'Select All',
+		combo: ['CMD', 'A']
+	},
+	{
+		title: 'Paste',
+		combo: ['CMD', 'V']
+	},
+	{
+		title: 'Refresh',
+		combo: ['CMD', 'R']
+	}
+];
 
-app.keyEvent = function() {
+app.activeKeys = [];
+app.currentCombo = {};
+
+
+app.renderKeys = function() {
+	var html = app.activeKeys.map(function(code) {
+		return `
+			<div class="key">${code}</div>
+		`
+	});
+	$('.keys').html(html.join(''));
+};
+
+app.randomCombo = function() {
+	var random = app.combos[Math.floor(Math.random() * app.combos.length)];
+	app.currentCombo = random;
+	$('.combo').html(random.title);
+}
+
+app.checkCombo = function() {
+	var keys = app.activeKeys.map(function(key,i) {
+		if(key === app.currentCombo.combo[i]) {
+			return true;
+		}
+	});
+	
+	app.activeKeys = [];
+}
+
+app.keyEvents = function() {
 	$(document).on('keydown', function(e) {
 		e.preventDefault();
+		app.activeKeys.push(app.keyCodes[e.keyCode]);
+		if(app.activeKeys.length > 1) {
+			app.checkCombo();
+		}
+		app.renderKeys();
 	});
 };
 
@@ -24,6 +83,7 @@ app.init = function() {
 	//Give them 2mins to get as many command right, each is 10 points
 	//When they get one wrong...lose points?
 	app.keyEvents();
+	app.randomCombo();
 };
 
 $(app.init);
